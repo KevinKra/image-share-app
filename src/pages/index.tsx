6fb95252dbfb8039e0f90dda66d524abc3a1930f
KevinKra <kratok08@gmail.com>
2022-02-05 10:32:12 -0700

    config cognito identity

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,41 @@
 import type { NextPage } from "next";
-import AWS from "aws-sdk";
+import { fromCognitoIdentityPool } from "@aws-sdk/credential-providers";
+import { S3, ListObjectsCommand } from "@aws-sdk/client-s3";
 
-AWS.config.region = "us-east-2"; // Region
-AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-  IdentityPoolId: "us-east-2:6fc7eebc-f732-4384-8041-a38f34178cb7",
+const credentials = fromCognitoIdentityPool({
+  identityPoolId: process.env.NEXT_PUBLIC_IDENTITY_POOL_ID || "",
+  clientConfig: { region: "us-east-2" },
 });
 
+const albumBucket = "kkrato-image-share";
+
+const client = new S3({
+  region: "us-east-2",
+  credentials,
+});
+
+const getBucketObjects = async () => {
+  const command = new ListObjectsCommand({
+    Bucket: albumBucket,
+  });
+
+  try {
+    const response = await client.send(command);
+    console.log("res", response);
+  } catch (error) {
+    console.log("error", error);
+  }
+};
+
 const Home: NextPage = () => {
+  const onClick = () => {
+    getBucketObjects();
+  };
+
   return (
     <div>
       <p>hello world</p>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 };
